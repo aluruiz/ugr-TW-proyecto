31112729e7287ea9794a1c065b259cf83857cf6d
Paula <32360877+aluruiz@users.noreply.github.com>
2019-05-28 12:49:15 +0200

    AñadidasPaginasHTML
    He puesto el HTML mas o menos, sin datos.
    Ademas he puesto el CSS este cutrecillo, que tengo que cambiarlo en cuanto pueda mirarmelo por encima.

---
+++ proyecto/nuevaIncidencia.php
@@ -0,0 +1,11 @@
+<?php
+require_once './controlador/herramientas/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('.');
+$twig = new \Twig\Environment($loader);
+
+$ruta = "vista/nuevaIncidencia.html";
+$template = $twig -> load($ruta);
+
+echo $template -> render();
+?>
