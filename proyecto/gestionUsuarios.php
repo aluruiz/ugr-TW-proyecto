5036218e91f83e1228f190f2e47b24c56208265b
Paula <32360877+aluruiz@users.noreply.github.com>
2019-07-06 19:05:23 +0200

    CSS terminado.

--- proyecto/gestionUsuarios.php
+++ proyecto/gestionUsuarios.php
@@ -4,10 +4,11 @@ require_once './core/BaseDeDatos.php';
 require_once './login.php';
 require_once './core/modelo/Aside.php';
 
-  $aside=new Aside(3);
+$aside=new Aside(3);
 $database = new Database();
 $idLogeado = getUsuarioLogged();
 $loggedUser = $database->getUsuarioById($idLogeado);
+$usuarios = $database->getUsuarios();
 
 $loader = new \Twig\Loader\FilesystemLoader('.');
 $twig = new \Twig\Environment($loader);
@@ -17,7 +18,8 @@ $template = $twig -> load($ruta);
 
 $argumentos = [];
 $argumentos["loggedUser"] = $loggedUser;
-  $argumentos["aside"]=$aside;
+$argumentos["aside"]=$aside;
+$argumentos["usuarios"]=$usuarios;
 
 echo $template -> render($argumentos);
 ?>
